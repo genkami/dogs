9aa73f77d75a2aa22dfa3bac72b40ca8663a8108
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-09-29 18:35:45 +0900

    Test: rename test for DefaultOrd

--- cmp_test.go
+++ cmp_test.go
@@ -6,42 +6,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestOrd_Lt(t *testing.T) {
+func TestDefaultOrd_Lt(t *testing.T) {
 	subject := intOrd.Lt
 	assert.True(t, subject(123, 124))
 	assert.False(t, subject(123, 123))
 	assert.False(t, subject(123, 122))
 }
 
-func TestOrd_Le(t *testing.T) {
+func TestDefaultOrd_Le(t *testing.T) {
 	subject := intOrd.Le
 	assert.True(t, subject(123, 124))
 	assert.True(t, subject(123, 123))
 	assert.False(t, subject(123, 122))
 }
 
-func TestOrd_Gt(t *testing.T) {
+func TestDefaultOrd_Gt(t *testing.T) {
 	subject := intOrd.Gt
 	assert.False(t, subject(123, 124))
 	assert.False(t, subject(123, 123))
 	assert.True(t, subject(123, 122))
 }
 
-func TestOrd_Ge(t *testing.T) {
+func TestDefaultOrd_Ge(t *testing.T) {
 	subject := intOrd.Ge
 	assert.False(t, subject(123, 124))
 	assert.True(t, subject(123, 123))
 	assert.True(t, subject(123, 122))
 }
 
-func TestOrd_Eq(t *testing.T) {
+func TestDefaultOrd_Eq(t *testing.T) {
 	subject := intOrd.Eq
 	assert.False(t, subject(123, 124))
 	assert.True(t, subject(123, 123))
 	assert.False(t, subject(123, 122))
 }
 
-func TestOrd_Ne(t *testing.T) {
+func TestDefaultOrd_Ne(t *testing.T) {
 	subject := intOrd.Ne
 	assert.True(t, subject(123, 124))
 	assert.False(t, subject(123, 123))
