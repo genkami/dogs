d743916df53421da70566037c3ade1dfdcdb409c
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-09-30 23:32:55 +0900

    Misc: add prelude

---
+++ dogs.go
@@ -0,0 +1,14 @@
+// Package dogs provides some types that are commonly used.
+package dogs
+
+// TODO: ./dogs.go:11:16: syntax error: generic type cannot be alias
+
+// type Eq[T any] = cmp.Eq[T]
+// type Ord[T any] = cmp.Ord[T]
+
+// type Semigroup[T any] = algebra.Semigroup[T]
+// type Monoid[T any] = algebra.Monoid[T]
+
+// type Pair[T, U any] = pair.Pair[T, U]
+
+// type Iterator[T any] = iterator.Iterator[T]
