a0ab279507cbde7f6273367d93fcd10abc6e9493
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-09-30 20:56:47 +0900

    Feat: add DeriveAdditiveSemigroup

--- algebra.go
+++ algebra.go
@@ -1,8 +1,26 @@
 package dogs
 
+import "constraints"
+
+// Additive is a type that can use `+` operator.
+type Additive interface {
+	constraints.Integer | constraints.Float | constraints.Complex | ~string
+}
+
 // Semigroup is a set of type `T` and its associative binary operation `Combine(T, T) T`
 type Semigroup[T any] interface {
 	Combine(T, T) T
+}
+
+// DeriveAdditiveSemigroup derives Semigroup using `+` operator.
+func DeriveAdditiveSemigroup[T Additive]() Semigroup[T] {
+	return additiveSemigroup[T]{}
+}
+
+type additiveSemigroup[T Additive] struct {}
+
+func (additiveSemigroup[T]) Combine(x, y T) T {
+	return x + y
 }
 
 // DefaultSemigroup is a default implementation of Semigroup.
--- algebra_test.go
+++ algebra_test.go
@@ -1,2 +1,13 @@
 package dogs_test
 
+import (
+	"github.com/genkami/dogs"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDeriveAdditiveSemigroup(t *testing.T) {
+	s := dogs.DeriveAdditiveSemigroup[int]()
+
+	assert.Equal(t, s.Combine(1, 2), 3)
+}
