c3989a63cac9cd816af767c4a2baf7958b3cc806
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-10-01 23:22:22 +0900

    Feat: add Set

--- types/set/set.go
+++ types/set/set.go
@@ -1,20 +1,77 @@
 package set
 
-// TODO: type Set[T]
-// TODO: New[T](elems ...T) Set[T]
-// TODO: FromIterator[T](it Iterator[T]) Set[T]
-// TODO: Len[T](s Set[T]) int
-// TODO: Has[T](s Set[T], e T) bool
-// TODO: Add[T](s Set[T], e T)
-// TODO: Remove[T](s Set[T], e T) bool
-// TODO: IterElems[T](s Set[T]) Iterator[T]
+import "github.com/genkami/dogs/types/iterator"
+
+// Set is a set of type T.
+// https://golang.org/ref/spec#Comparison_operators
+type Set[T comparable] map[T]struct{}
+
+// New creates a new Set.
+func New[T comparable](elems ...T) Set[T] {
+	s := Set[T]{}
+	for _, e := range elems {
+		Add[T](s, e)
+	}
+	return s
+}
+
+// FromIterator returns a Set from given Iterator.
+func FromIterator[T comparable](it iterator.Iterator[T]) Set[T] {
+	s := Set[T]{}
+	for {
+		x, ok := it.Next()
+		if !ok {
+			break
+		}
+		Add[T](s, x)
+	}
+	return s
+}
+
+// Iter returns an Iterator that iterates over s.
+func (s Set[T]) Iter() iterator.Iterator[T] {
+	keys := make([]T, 0, len(s))
+	for k, _ := range s {
+		keys = append(keys, k)
+	}
+	return iterator.Unfold[int, T](0, func(i int) (int, T, bool) {
+		if len(keys) <= i {
+			var zero T
+			return 0, zero, false
+		}
+		return i + 1, keys[i], true
+	})
+}
+
+
+
+// Add adds an element e to set s.
+func Add[T comparable](s Set[T], e T) {
+	s[e] = struct{}{}
+}
+
+// Remove removes an element e from set s.
+// If returns false if and only if s doesn't have e.
+func Remove[T comparable](s Set[T], e T) bool {
+	if _, ok := s[e]; !ok {
+		return false
+	}
+	delete(s, e)
+	return true
+}
+
+// Has returns true if and only if s has e.
+func Has[T comparable](s Set[T], e T) bool {
+	_, ok := s[e]
+	return ok
+}
+
+// Len returns the number of elements in s.
+func Len[T comparable](s Set[T]) int {
+	return len(s)
+}
+
 // TODO: Elems[T](s Set[T]) []T
 // TODO: Merge[T](s, t Set[T]) Set[T]
 // TODO: Union[T](s, t Set[T]) Set[T]
 // TODO: Intersection[T](s, t Set[T]) Set[T]
-
-// TODO: Map[T, U](s Set[T], fn func(T) U) Set[U]
-// TODO: Fold[T, U](init T, s Set[U], fn func(T, U) T) T
-// TODO: ForEach[T](s Set[T], fn func(T))
-// TODO: SumWithInit[T](init T, s Set[T], semi Semigroup[T]) T
-// TODO: Sum[T](s Set[T], m Monoid[T]) T
